35eb80737744fa0292305822fd79b91024e83914
Patrick Ferber <ferber@cs.uni-saarland.de>
2019-12-12 12:44:05 +0100

    conversion script supports being softlinked

--- 2019-12-Cleanup/run.sh
+++ 2019-12-Cleanup/run.sh
@@ -4,6 +4,6 @@ if [ $# -ne 2 ]; then
   echo "Invalid arguments. Use: $0 SRC DST"
   exit 1
 fi
-BASE=`dirname $0`
+BASE=$(dirname $(readlink -f $0))
 hg convert $1 $2 --authormap "$BASE/downward_authormap.txt" --filemap "$BASE/downward_filemap.txt"
 
