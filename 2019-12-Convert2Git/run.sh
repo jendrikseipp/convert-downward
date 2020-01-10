6b1eee0b1a87b659d27c47dfd5663cd9fe82f3c4
Patrick Ferber <ferber@cs.uni-saarland.de>
2020-01-10 15:29:59 +0100

    add conversion of ignore files to run.sh

--- 2019-12-Convert2Git/run.sh
+++ 2019-12-Convert2Git/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Verify correct inputs
 if [ $# -lt 2 ] || [ $# -gt 3 ]; then
   echo "Invalid arguments. Use: $0 SRC DST [REMOTE-GIT-ORIGIN]"
   exit 1
@@ -12,11 +13,16 @@ if [ -e $2 ]; then
   echo "Destination exists: $2"
   exit 1
 fi
+
+# Setup paths
 SRC=`realpath $1`
 DST=`realpath $2`
-
 BASE=$(realpath $(dirname $(readlink -f $0)))
+CONVERT_HGIGNORE="${BASE}/convert_hgignore_to_gitignore.py"
+MAPPING_HGIGNORE="${BASE}/ignore_map.txt"
 FAST_EXPORT_REPO=${FAST_EXPORT_REPO:-"$BASE/fast-export"}
+
+# Download Mercurial to Git conversion tool
 if [ ! -d $FAST_EXPORT_REPO ]; then
     git clone https://github.com/frej/fast-export.git $FAST_EXPORT_REPO
     cd $FAST_EXPORT_REPO
@@ -25,13 +31,41 @@ fi
 FAST_EXPORT="$FAST_EXPORT_REPO/hg-fast-export.sh"
 echo "fast-export location: $FAST_EXPORT"
 
-
-
+# Convert Mercurial to Git
 mkdir -p $DST
 cd $DST
 git init
 $FAST_EXPORT -r $SRC
 git checkout
+
+# Convert .hgignore to .gitignore
+BRANCHES=$(hg -R ${SRC} branches)
+BRANCHES=`python3 -c "print(' '.join(\
+  'master' if name == 'default' else name \
+  for no, name in enumerate('''$(hg -R ${SRC} branches)'''.split()) \
+  if no % 2 == 0))"`
+HGIGNORE="${DST}/.hgignore"
+for BRANCH in $BRANCHES; do
+  git checkout $BRANCH
+  if [ -e $HGIGNORE ]; then
+    python ${CONVERT_HGIGNORE} $HGIGNORE --ignoremap ${MAPPING_HGIGNORE} > ".gitignore"
+    git add ".gitignore"
+    git rm ".hgignore"
+    git commit -m "Automatically converted .hgignore to .gitignore"
+  fi
+done
+
+
+# Todo: Close branches in git, if possible
+
+#Archive (maybe our new close) branch
+#git tag archive/<branchname> <branchname>
+#git branch -d <branchname>
+#git checkout master
+#
+## Reopen branch
+#git checkout -b new_branch_name archive/<branchname>
+
 
 if [ $# -eq 3 ]; then
     git remote add origin $3
