305c07327ed50d46342a53fc1dfb8f42c6546cc7
Patrick Ferber <patrick.ferber@unibas.ch>
2020-07-06 12:31:07 +0200

    add script to run both cleanup and conversion

--- run_cleanup_and_conversion.sh
+++ run_cleanup_and_conversion.sh
@@ -2,61 +2,51 @@
 
 set -euo pipefail
 
-MISSING_REQUIREMENTS=""
-
-if ! command -v python3 > /dev/null; then
-    MISSING_REQUIREMENTS="${MISSING_REQUIREMENTS}\nMissing requirement: python3.5+"
-elif [[ `python3 -c "import sys; print(sys.version_info < (3,5))"` = "True" ]];then
-        MISSING_REQUIREMENTS="${MISSING_REQUIREMENTS}\nMissing requirement: python3.5+"
+if [[ $# -ne 3 ]]; then
+  echo "Invalid arguments. Use: $0 [SRC REPOSITORY] [CLEANED REPOSITORY] [CONVERTED REPOSITORY]"
+  exit 1
 fi
 
-if ! `python3 -c "import ensurepip" 2> /dev/null`; then
-    MISSING_REQUIREMENTS="${MISSING_REQUIREMENTS}\nMissing requirement: \
-ensurepip module missing for python3. For Debian/Ubuntu use \
-'sudo apt install python3-venv'"
+if [[ ! -d $1 ]]; then
+  echo "Invalid argument. $1 has to be a directory."
+  exit 1
 fi
 
-if [[ `dpkg-query -f '${Package}\n' -W | grep "python3-dev"` == "" ]];then
-    MISSING_REQUIREMENTS="${MISSING_REQUIREMENTS}\nMissing requirement: python3-dev"
+if [[ -e $2 ]]; then
+  echo "Invalid argument. $2 may not exist."
+  exit 1
 fi
 
-if ! command -v git > /dev/null; then
-    MISSING_REQUIREMENTS="${MISSING_REQUIREMENTS}\nMissing requirement: git"
+if [[ -e $3 ]]; then
+  echo "Invalid argument. $3 may not exist."
+  exit 1
 fi
-
-if [[ ${MISSING_REQUIREMENTS} != "" ]]; then
-    echo -e ${MISSING_REQUIREMENTS}
-    exit 3
-fi
-
 
 
 
 BASE=$(realpath $(dirname $(readlink -f $0)))
-DATA="${BASE}/data"
-VIRTUALENV="${DATA}/py3-env"
-FAST_EXPORT_REPO="${DATA}/fast-export"
-CONVERT="${BASE}/convert.py"
+SETUP_CLEANUP="${BASE}/setup_cleanup.sh"
+SETUP_CONVERSION="${BASE}/setup_conversion.sh"
+RUN_CLEANUP="${BASE}/run_cleanup.sh"
+RUN_CONVERSION="${BASE}/run_conversion.sh"
 
-MERCURIAL_VERSION="mercurial==5.2"
-FAST_EXPORT_VERSION="v200213-23-g44c50d0"
-
-echo "Setup python virtual environment"
-if [[ ! -d ${VIRTUALENV} ]]; then
-    python3 -m venv ${VIRTUALENV}
-    source "$VIRTUALENV/bin/activate"
-    pip install --upgrade pip wheel
-    pip install ${MERCURIAL_VERSION}
-    echo `hg --version | grep "version"`
-else
-    source "$VIRTUALENV/bin/activate"
+if ! /bin/bash ${SETUP_CLEANUP}; then
+  echo "Error during the setup for the cleaning script."
+  exit 2
 fi
 
-echo "Setup fast-export"
-if [[ ! -d ${FAST_EXPORT_REPO} ]]; then
-    git clone https://github.com/frej/fast-export.git ${FAST_EXPORT_REPO}
-    git -C ${FAST_EXPORT_REPO} checkout ${FAST_EXPORT_VERSION}
+if ! /bin/bash ${SETUP_CONVERSION}; then
+  echo "Error during the setup for the conversion script."
+  exit 2
 fi
-export FAST_EXPORT_REPO=${FAST_EXPORT_REPO}
 
-python3 ${CONVERT} $@
+if ! ${RUN_CLEANUP} $1 $2; then
+  echo "Cleanup failed."
+  exit 2
+fi
+
+if ! ${RUN_CONVERSION} $2 $3; then
+  echo "Conversion failed."
+  exit 2
+fi
+
